0d0a30a2d133b9c361794fd027bcc0e338f2866e
SHIVAM AGRAHARI <iec2019087@iiita.ac.in>
2020-08-09 20:12:27 +0530

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,21 @@
+$('ul').on('click', 'li', function() {
+	$(this).toggleClass('completed');
+});
+
+$('ul').on('click', 'span', function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").on('keypress', function(event) {
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val('');
+		$('ul').append("<li><span id='btn'><i class='fas fa-trash'></i></span> " + todoText + '</li>');
+	}
+});
+$('#toggle-form').click(function() {
+	$("input[type='text']").fadeToggle(100);
+});
